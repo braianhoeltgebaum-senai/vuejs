64b1046559c720137d4b7ecf8da63c08547ff92b
Braian Hoeltgebaum <braian_hoeltgebaum@estudante.sesisenai.org.br>
2026-05-19 20:44:05 -0300

    .

--- src/main/java/com/kanban/vuejs/backendspring/controller/GerenciadorController.java
+++ src/main/java/com/kanban/vuejs/backendspring/controller/GerenciadorController.java
@@ -1,5 +1,51 @@
 package com.kanban.vuejs.backendspring.controller;
 
+import java.util.List;
+import com.kanban.vuejs.backendspring.model.Gerenciador;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kanban.vuejs.backendspring.service.GerenciadorService;
+
+
+@RestController
+@RequestMapping("/tarefas")
+@CrossOrigin("*")
 public class GerenciadorController {
 
+    private final GerenciadorService service;
+
+    public GerenciadorController(GerenciadorService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Gerenciador> listar() {
+        return service.listar();
+    }
+
+    @PostMapping
+    public Gerenciador criar(@RequestBody Gerenciador gerenciador) {
+        return service.salvar(gerenciador);
+    }
+
+    @PutMapping("/{id}")
+    public Gerenciador atualizar(
+            @PathVariable Long id,
+            @RequestBody Gerenciador gerenciador) {
+
+        return service.atualizar(id, gerenciador);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        service.deletar(id);
+    }
 }
--- src/main/java/com/kanban/vuejs/backendspring/service/GerenciadorService.java
+++ src/main/java/com/kanban/vuejs/backendspring/service/GerenciadorService.java
@@ -1,5 +1,41 @@
 package com.kanban.vuejs.backendspring.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.kanban.vuejs.backendspring.model.Gerenciador;
+import com.kanban.vuejs.backendspring.repository.GerenciadorRepository;
+
+@Service
 public class GerenciadorService {
 
+    private final GerenciadorRepository repository;
+
+    public GerenciadorService(GerenciadorRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Gerenciador> listar() {
+        return repository.findAll();
+    }
+
+    public Gerenciador salvar(Gerenciador gerenciador) {
+        return repository.save(gerenciador);
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
+
+    public Gerenciador atualizar(Long id, Gerenciador gerenciadorAtualizada) {
+
+        Gerenciador gerenciador = repository.findById(id)
+                .orElseThrow();
+
+        gerenciador.setText(gerenciadorAtualizada.getText());
+        gerenciador.setCompleted(gerenciadorAtualizada.isCompleted());
+
+        return repository.save(gerenciador);
+    }
 }
