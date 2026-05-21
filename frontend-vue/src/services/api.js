e5cd16ffc4f10a176a79e312cb84a131f2f2039d
Caíque Oliveira <caique_oliveira@estudante.sc.senai.br>
2026-05-21 19:42:22 -0300

    Corrigindo conexão remota

--- frontend-vue/src/services/api.js
+++ frontend-vue/src/services/api.js
@@ -1,9 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-<<<<<<< Updated upstream
-  baseURL: "http://10.74.241.64:8080",
-=======
   baseURL: "http://10.74.241.64:8088",
->>>>>>> Stashed changes
 });
--- frontend-vue/vite.config.js
+++ frontend-vue/vite.config.js
@@ -3,4 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true,
+    port: 5173
+  }
 })
