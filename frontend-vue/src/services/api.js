8ffee37dbec60463f22ef99ceae430965d8ba8f2
Caíque Oliveira <caique_oliveira@estudante.sc.senai.br>
2026-05-21 19:38:58 -0300

    Update api.js

--- frontend-vue/src/services/api.js
+++ frontend-vue/src/services/api.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
+<<<<<<< Updated upstream
   baseURL: "http://10.74.241.64:8080",
+=======
+  baseURL: "http://10.74.241.64:8088",
+>>>>>>> Stashed changes
 });
